32fc5e51ec9e75fe5cbd732510fced2f6c7bb8c6
LTprograms <105554066+LTprograms@users.noreply.github.com>
2023-01-20 22:56:00 -0500

    conteoFinal

--- js/conteo.js
+++ js/conteo.js
@@ -1,58 +1,93 @@
-var questions = []
-var good = 20;
-var bad = 1.125;
+var ptj = 0;
+var good = 20; //1
+var bad = 1.125; //-1
+var questions = []; //0 = blank
+//btns
+var btnGood, btnBad, btnBlank, btnBack;
+//lists
+var ul1;
+//td
+var ptjTd, goodTd, badTd, blankTd, totalTd;
+document.addEventListener("load", ready, true);
 function ready() {
-    let btnGood = document.getElementById("btn-good");
-    let btnBad = document.getElementById("btn-bad");
-    let btnBack = document.getElementById("btn-back");
+    //btns
+    btnGood = document.getElementById("btn-good");
+    btnBad = document.getElementById("btn-bad");
+    btnBlank = document.getElementById("btn-blank");
+    btnBack = document.getElementById("btn-back");
+    //lists
+    ul1 = document.getElementById("ul1");
+    //td
+    ptjTd = document.getElementById("ptj");
+    goodTd = document.getElementById("good");
+    badTd = document.getElementById("bad");
+    blankTd = document.getElementById("blank");
+    totalTd = document.getElementById("total");
+    //events
     btnGood.addEventListener("click", goodFun, true);
     btnBad.addEventListener("click", badFun, true);
+    btnBlank.addEventListener("click", blankFun, true);
     btnBack.addEventListener("click", backFun, true);
-}
-function goodFun() {
-    if (questions.length<100) {        
-        questions.push(true);
-        update();
-    }
-}
-function badFun() {
-    if (questions.length<100) {        
-        questions.push(false);
-        update();
-    }
-}
-function backFun() {
-    if (questions.length<=100) {        
-        questions.pop();
-        update();
-    }    
-}
-function reset(ptj, g, b, t) {
-    ptj.textContent = 0;
-    g.textContent = 0;
-    b.textContent = 0;
-    t.textContent = 0;
-}
-function update() {
-    let ptj = document.getElementById("ptj");
-    let goodSpan = document.getElementById("good");
-    let badSpan = document.getElementById("bad");
-    let total = document.getElementById("total");
-    let g = 0;
-    let b = 0;
-         
-    reset(ptj, goodSpan, badSpan, total);
-    for (let i = 0; i < questions.length; i++) {        
-        if (questions[i]) {
-            goodSpan.textContent = parseInt(goodSpan.textContent) + 1;
-            g++;
-        }else{
-            badSpan.textContent = parseInt(badSpan.textContent) + 1;
-            b++;
-        }
-        total.textContent = parseInt(total.textContent) + 1;
-    }
-    ptj.textContent = g*good - b*bad;
     
 }
-document.addEventListener("load", ready, true);
+function goodFun() {
+    ptj+=good;
+    ptjTd.textContent = ptj;
+    goodTd.textContent=parseInt(goodTd.textContent)+1;
+    totalTd.textContent=parseInt(totalTd.textContent)+1;
+    questions.push(1);
+    appendLi(1);
+}
+function badFun() {
+    ptj-=bad;
+    ptjTd.textContent = ptj;
+    badTd.textContent=parseInt(badTd.textContent)+1;
+    totalTd.textContent=parseInt(totalTd.textContent)+1;
+    questions.push(-1);
+    appendLi(-1);
+}
+function blankFun() {
+    blankTd.textContent=parseInt(blankTd.textContent)+1;
+    totalTd.textContent=parseInt(totalTd.textContent)+1;
+    questions.push(0);
+    appendLi(0);
+}
+function backFun() {
+    if (questions.length!=0) {        
+        let last = questions[questions.length-1];
+        questions.pop();
+        switch (last) {
+            case 1: 
+                ptj-=good;
+                goodTd.textContent=parseInt(goodTd.textContent)-1;            
+                break;
+            case 0: 
+                blankTd.textContent=parseInt(blankTd.textContent)-1;
+                break;
+            case -1: 
+                ptj+=bad;
+                badTd.textContent=parseInt(badTd.textContent)-1;
+                break;
+        }
+        ptjTd.textContent = ptj;
+        totalTd.textContent=parseInt(totalTd.textContent)-1;
+        ul1.lastChild.remove();
+    }
+}
+function appendLi(type) {
+    let node = document.createElement("li");
+    node.textContent = questions.length;
+    node.textContent += ". ";
+    switch (type) {
+        case 1: 
+            node.textContent += "Buena"; 
+            node.className="text-success"; 
+            break;
+        case 0: node.textContent += "Blanco"; break;
+        case -1: 
+            node.textContent += "Mala";
+            node.className="text-danger"; 
+            break;
+    }
+    ul1.appendChild(node);
+}
